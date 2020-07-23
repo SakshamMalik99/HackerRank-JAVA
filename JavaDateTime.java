cd39eabcd0ccc27efa8487c85a21ce6de6920f01
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-07-24 01:38:28 +0530

    Update JavaDateTime.java

--- JavaDateTime.java
+++ JavaDateTime.java
@@ -18,7 +18,7 @@ class Result {
 
 }
 
-public class JavaDateTime {
+public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
